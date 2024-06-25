598f6c5de16cfd4dc6f2b952bc7fb1573c9492bc
Vigneshwaran S <168060891+Vigneshwarrs@users.noreply.github.com>
2024-06-25 11:50:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ display.append(h1);
 
 setTimeout(()=>{
     h1.innerText = `Counter: ${10}`;
-    setTimeout(()={
+    setTimeout(()=>{
         h1.innerText = `Counter: ${9}`;
-        setTimeout(()={
+        setTimeout(()=>{
             h1.innerText = `Counter: ${8}`;
-            setTimeout(()={
+            setTimeout(()=>{
                 h1.innerText = `Counter: ${7}`;
-                setTimeout(()={
+                setTimeout(()=>{
                     h1.innerText = `Counter: ${6}`;
-                    setTimeout(()={
+                    setTimeout(()=>{
                         h1.innerText = `Counter: ${5}`;
-                        setTimeout(()={
+                        setTimeout(()=>{
                             h1.innerText = `Counter: ${4}`;
-                            setTimeout(()={
+                            setTimeout(()=>{
                                 h1.innerText = `Counter: ${3}`;
-                                setTimeout(()={
+                                setTimeout(()=>{
                                     h1.innerText = `Counter: ${2}`;
-                                    setTimeout(()={
+                                    setTimeout(()=>{
                                         h1.innerText = `Counter: ${1}`;
-                                        setTimeout(()={
+                                        setTimeout(()=>{
                                             h1.innerText = "Happy Independance Day";
                                         },1000);
                                     },1000);
